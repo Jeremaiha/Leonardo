a429e5fdad2484ab978223ce06278f2d0a8d0e7c
Jeremy Shiklov <myipodspain@gmail.com>
2015-08-30 23:26:03 +0300

    Image Button Activity
    Added the ability to move to a new screen when clicking the image
    button.

---
+++ Leonardo/Game.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Leonardo
+{
+    [Activity(Label = "Game")]
+    public class Game : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Create your application here
+
+        }
+    }
+}
--- Leonardo/MainActivity.cs
+++ Leonardo/MainActivity.cs
@@ -22,9 +22,11 @@ namespace Leonardo
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            ImageButton button = FindViewById<ImageButton>(Resource.Id.imageButton1);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += delegate {
+                StartActivity(typeof(Game));
+            };
         }
     }
 }
