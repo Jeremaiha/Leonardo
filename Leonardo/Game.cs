7de16e59185ace2f333a8834698a3cbecd7c68c8
Jeremy Shiklov <myipodspain@gmail.com>
2015-08-31 18:36:55 +0300

    Successfully created the click event

--- Leonardo/Game.cs
+++ Leonardo/Game.cs
@@ -15,15 +15,31 @@ namespace Leonardo
     [Activity(Label = "Game")]
     public class Game : Activity
     {
+        const int SIZE = 4;
         ImageButton outerImageButton;
+        ImageButton[,] buttonsArray;
+
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Create your application here
+            // Layout initialisation
             SetContentView(Resource.Layout.Game);
 
             outerImageButton = FindViewById<ImageButton>(Resource.Id.imageButton17);
+            outerImageButton.SetImageResource(Resource.Drawable.red_mushroom_1);
+            
+            //tempButton = FindViewById<ImageButton>(Resource.Id.imageButton14);
+
+/*            tempButton.Click += (sender, e) => {
+                tempButton.SetImageResource(Resource.Drawable.green_cherry_3);
+            };
+            */
         }
+
+
+
     }
 }
