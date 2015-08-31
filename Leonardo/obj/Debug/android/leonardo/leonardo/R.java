0f32937a854e5271f6cab154b56a80d70d622a3b
Jeremy Shiklov <myipodspain@gmail.com>
2015-08-31 16:52:58 +0300

    Created the buttons table

--- Leonardo/obj/Debug/android/leonardo/leonardo/R.java
+++ Leonardo/obj/Debug/android/leonardo/leonardo/R.java
@@ -15,9 +15,30 @@ public final class R {
         public static final int play_icon_small=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f050002;
-        public static final int button2=0x7f050003;
-        public static final int imageButton1=0x7f050001;
+        public static final int button1=0x7f050017;
+        public static final int button2=0x7f050018;
+        public static final int imageButton1=0x7f050003;
+        public static final int imageButton10=0x7f05000e;
+        public static final int imageButton11=0x7f05000f;
+        public static final int imageButton12=0x7f050010;
+        public static final int imageButton13=0x7f050012;
+        public static final int imageButton14=0x7f050013;
+        public static final int imageButton15=0x7f050014;
+        public static final int imageButton16=0x7f050015;
+        public static final int imageButton17=0x7f050016;
+        public static final int imageButton2=0x7f050004;
+        public static final int imageButton3=0x7f050005;
+        public static final int imageButton4=0x7f050006;
+        public static final int imageButton5=0x7f050008;
+        public static final int imageButton6=0x7f050009;
+        public static final int imageButton7=0x7f05000a;
+        public static final int imageButton8=0x7f05000b;
+        public static final int imageButton9=0x7f05000d;
+        public static final int tableLayout1=0x7f050001;
+        public static final int tableRow1=0x7f050002;
+        public static final int tableRow2=0x7f050007;
+        public static final int tableRow3=0x7f05000c;
+        public static final int tableRow4=0x7f050011;
         public static final int textView1=0x7f050000;
     }
     public static final class layout {
