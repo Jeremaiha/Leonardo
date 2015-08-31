b28e268f3a6c56272d6761ea680c072603e18cda
Jeremy Shiklov <myipodspain@gmail.com>
2015-08-31 21:18:34 +0300

    Card class.
    Created the card class.

--- Leonardo/Game.cs
+++ Leonardo/Game.cs
@@ -16,51 +16,53 @@ namespace Leonardo
     public class Game : Activity
     {
         const int SIZE = 4;
-        ImageButton outerImageButton;
-        ImageButton[,] buttonsArray;
-        HashSet<Resource.Drawable> gameImages;
+        Card outerImageButton;
+        Card[,] buttonsArray;
+        Card[] gameImages;
 
         private void initiateAll(){
             initiateButtonsArray();
             initiateSetOfImages();
         }
 
+        /// <summary>
+        ///     Create an array of all images.
+        /// </summary>
         private void initiateSetOfImages(){
-            gameImages = new HashSet<Resource.Drawable>();
-           // var temp = Resource.Drawable;
+            gameImages = new Card[SIZE];
 
-          //  gameImages.Add(temp);
+           
         }
 
         /// <summary>
         ///     Initiating the 2 dimentional array of buttons with a blank image.
         /// </summary>
         private void initiateButtonsArray(){
-            buttonsArray = new ImageButton[SIZE, SIZE];
+            buttonsArray = new Card[SIZE, SIZE];
             
-            buttonsArray[0, 0] = FindViewById<ImageButton>(Resource.Id.imageButton1);
-            buttonsArray[0, 1] = FindViewById<ImageButton>(Resource.Id.imageButton2);
-            buttonsArray[0, 2] = FindViewById<ImageButton>(Resource.Id.imageButton3);
-            buttonsArray[0, 3] = FindViewById<ImageButton>(Resource.Id.imageButton4);
+            buttonsArray[0, 0] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton1),"blank","white",0);
+            buttonsArray[0, 1] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton2), "blank", "white", 0);
+            buttonsArray[0, 2] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton3), "blank", "white", 0);
+            buttonsArray[0, 3] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton4), "blank", "white", 0);
 
-            buttonsArray[1, 0] = FindViewById<ImageButton>(Resource.Id.imageButton5);
-            buttonsArray[1, 1] = FindViewById<ImageButton>(Resource.Id.imageButton6);
-            buttonsArray[1, 2] = FindViewById<ImageButton>(Resource.Id.imageButton7);
-            buttonsArray[1, 3] = FindViewById<ImageButton>(Resource.Id.imageButton8);
+            buttonsArray[1, 0] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton5), "blank", "white", 0);
+            buttonsArray[1, 1] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton6), "blank", "white", 0);
+            buttonsArray[1, 2] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton7), "blank", "white", 0);
+            buttonsArray[1, 3] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton8), "blank", "white", 0);
 
-            buttonsArray[2, 0] = FindViewById<ImageButton>(Resource.Id.imageButton9);
-            buttonsArray[2, 1] = FindViewById<ImageButton>(Resource.Id.imageButton10);
-            buttonsArray[2, 2] = FindViewById<ImageButton>(Resource.Id.imageButton11);
-            buttonsArray[2, 3] = FindViewById<ImageButton>(Resource.Id.imageButton12);
+            buttonsArray[2, 0] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton9), "blank", "white", 0);
+            buttonsArray[2, 1] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton10), "blank", "white", 0);
+            buttonsArray[2, 2] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton11), "blank", "white", 0);
+            buttonsArray[2, 3] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton12), "blank", "white", 0);
 
-            buttonsArray[3, 0] = FindViewById<ImageButton>(Resource.Id.imageButton13);
-            buttonsArray[3, 1] = FindViewById<ImageButton>(Resource.Id.imageButton14);
-            buttonsArray[3, 2] = FindViewById<ImageButton>(Resource.Id.imageButton15);
-            buttonsArray[3, 3] = FindViewById<ImageButton>(Resource.Id.imageButton16);
+            buttonsArray[3, 0] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton13), "blank", "white", 0);
+            buttonsArray[3, 1] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton14), "blank", "white", 0);
+            buttonsArray[3, 2] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton15), "blank", "white", 0);
+            buttonsArray[3, 3] = new Card(FindViewById<ImageButton>(Resource.Id.imageButton16), "blank", "white", 0);
         
             for(int i=0;i<SIZE;i++){
                 for(int j=0;j<SIZE;j++){
-                    buttonsArray[i, j].SetImageResource(Resource.Drawable.blank);        
+                    buttonsArray[i, j].ImageButton.SetImageResource(Resource.Drawable.blank);        
                 }
             }
     }
@@ -94,6 +96,50 @@ namespace Leonardo
 
 
     }
+    /// <summary>
+    ///     Card class which is a card container.
+    ///     Contains the imageButton,Color,Shape and Amount.
+    /// </summary>
+    public class Card
+    {
+        // Define fields.
+        ImageButton imageButton;
+        string shape;
+        string color;
+        int amount;
+
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        /// <param name="ib">ImageButton </param>
+        /// <param name="shp">Shape</param>
+        /// <param name="clr">Color</param>
+        /// <param name="amnt">Amount</param>
+        public Card(ImageButton ib,string shp, string clr, int amnt){
+            imageButton = ib;
+            shape = shp;
+            color = clr;
+            amount = amnt;
+        }
+
+        // Define properties.
+        public ImageButton ImageButton{
+            get { return imageButton; }
+            set { imageButton = value;}
+        }
+        public string Shape{
+            get{ return shape; }
+            set{ shape = value;}
+        }
+        public string Color{
+            get{ return color; }
+            set{ color = value;}
+        }
+        public int Amount{
+            get{ return amount; }
+            set{ amount = value;}
+        }
 
 
+    }
 }
