56b8c6f5bee868eb485ff4864d1d6b4687342277
Jeremy Shiklov <myipodspain@gmail.com>
2015-09-18 16:09:34 +0300

    Added the columns score

--- Leonardo/Game.cs
+++ Leonardo/Game.cs
@@ -216,6 +216,7 @@ namespace Leonardo
                 numberOfCards--;
             }else{
                 // Game ended, show scores.
+                gameOver();
                 return;
             }
                
@@ -235,6 +236,16 @@ namespace Leonardo
             // Start all game rules validations.
             score.Text = (Int32.Parse(score.Text.ToString())+gameRules.simulateAllRules()).ToString();
             
+        }
+
+        private void gameOver(){
+            var callDialog = new AlertDialog.Builder(this);
+            callDialog.SetMessage("Game Over.\nYour Score is : " + score.Text);
+            callDialog.SetNeutralButton("OK", delegate { 
+                
+            });
+            callDialog.Show();
+            base.OnBackPressed();
         }
 
         protected override void OnCreate(Bundle bundle)
@@ -283,12 +294,6 @@ namespace Leonardo
                 buttonsArray[0, 0].Shape = outerImage.Shape;
                 buttonsArray[0, 0].Amount = outerImage.Amount;
                 buttonsArray[0, 0].Color = outerImage.Color;
-               /* var callDialog = new AlertDialog.Builder(this);
-                callDialog.SetMessage(buttonsArray[0, 0].Shape + "," + buttonsArray[0, 0].Amount.ToString() + " " + 
-                    buttonsArray[0, 0].Color);
-                callDialog.SetNeutralButton("ok", delegate { });
-                callDialog.Show();
-                */
                 randomNextCard();
             };
             buttonsArray[0, 1].ImageButton.Click += (sender, e) =>
@@ -546,9 +551,97 @@ namespace Leonardo
         /// <returns></returns>
         public int simulateAllRules(){
             int sum = 0;
-            sum = checkRows();
-         //   score.Text += sum;
+            sum = checkColumns();
+            sum += checkRows();
+            
+            //   score.Text += sum;
 
+            return sum;
+        }
+        /// <summary>
+        ///     Returns total sum of all columns.
+        /// </summary>
+        /// <returns></returns>
+        private int checkColumns(){
+            int sum = 0;
+            for (int i = 0; i < SIZE; i++){
+                sum += checkColumn(i);
+            }
+            return sum;
+        }
+        /// <summary>
+        ///     Check an individual column.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        private int checkColumn(int i)
+        {
+            int sum = 0, cnt = 0, columnSuccess = 0; //if it's at least 1, then we erase row. 3 is extra points.
+            string tempStr;
+            int tempInt;
+
+            // Check if there is at least 1 blank
+
+            for (int j = 0; j < SIZE; j++){ //checked with the rest.
+                if (gameBoard[j, i].Shape == "blank"){
+                    // at least 1 blank exists.
+                    return 0;
+                }
+            }
+
+            // Check amount.
+            tempInt = gameBoard[0, i].Amount; //took the first.
+            for (int j = 1; j < SIZE; j++){ //checked with the rest.
+                if (gameBoard[j, i].Amount == tempInt){
+                    cnt++;
+                }
+            }
+            if (cnt == 3){ //Successfull row.
+                sum += 50;
+                columnSuccess += 1;
+            }
+            // Check color.
+            cnt = 0;
+            tempStr = gameBoard[0, i].Color; //took the first.
+            for (int j = 1; j < SIZE; j++){ //checked with the rest.
+                if (gameBoard[j, i].Color == tempStr){
+                    cnt++;
+                }
+            }
+            if (cnt == 3){ //Successfull row.
+                sum += 50;
+                columnSuccess += 1;
+            }
+            // Check shape.
+            cnt = 0;
+            tempStr = gameBoard[0, i].Shape; //took the first.
+            for (int j = 1; j < SIZE; j++){ //checked with the rest.
+                if (gameBoard[j, i].Shape == tempStr){
+                    cnt++;
+                }
+            }
+            if (cnt == 3){ //Successfull row.
+                sum += 50;
+                columnSuccess += 1;
+            }
+
+            // If bonus needed.
+            if (columnSuccess == 3){
+                sum *= 2;
+            }
+
+            // If at least 1 row exists.
+            if (columnSuccess > 0)
+            {
+                for (int j = 0; j < SIZE; j++)
+                {
+                    gameBoard[j, i].ImageButton.SetImageResource(Resource.Drawable.blank);
+                    gameBoard[j, i].Shape = "blank";
+                    gameBoard[j, i].Color = "white";
+                    gameBoard[j, i].Amount = 0;
+                    gameBoard[j, i].ImageButton.Enabled = true;
+                }
+            }
             return sum;
         }
 
