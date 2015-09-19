6426579b632d9fb66473e117a9a994d1f9ddd4b0
Jeremy Shiklov <myipodspain@gmail.com>
2015-09-19 19:52:04 +0300

    Sign Up works without Parse

--- Leonardo/SignUp.cs
+++ Leonardo/SignUp.cs
@@ -21,8 +21,40 @@ namespace Leonardo
 
             // Create your application here
             SetContentView(Resource.Layout.SignUp);
-
+            Button submitBtn = FindViewById<Button>(Resource.Id.buttonSubmit);
+            sumbitClick(submitBtn);
         }
+
+        private void sumbitClick(Button submitBtn){
+            submitBtn.Click += (sender, e) =>{
+                User user = getUser();
+                if (user == null){
+                    var callDialog = new AlertDialog.Builder(this);
+                    callDialog.SetMessage("Wrong Email Address!");
+                    callDialog.SetNeutralButton("Woops", delegate{});
+                    callDialog.Show();
+                }else{
+                    Toast.MakeText(this,user.Name+" Was Added",ToastLength.Short).Show();
+                    StartActivity(typeof(MainActivity));
+                }
+            };
+        }
+
+        private User getUser(){
+            try{
+                var name = FindViewById<EditText>(Resource.Id.editText1);
+                var email = FindViewById<EditText>(Resource.Id.editText2);
+                var password = FindViewById<EditText>(Resource.Id.editText3);
+
+                return new User(name.Text, email.Text, password.Text);        
+            }
+            catch (FormatException){
+                return null;
+            }catch (Exception e){
+                throw new Exception("Error : Creation of a user.\n" + e.Message);
+            }
+        }
+
     }
 
 }
