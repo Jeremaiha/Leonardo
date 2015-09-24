c69746fdfeda7feefa693d445af1591630bd1594
Jeremy Shiklov <myipodspain@gmail.com>
2015-09-24 23:39:04 +0300

    Right and Left Diagonals are added.

--- Leonardo/GameRules.cs
+++ Leonardo/GameRules.cs
@@ -58,7 +58,7 @@ namespace Leonardo
         /// </summary>
         /// <returns></returns>
         public int checkRowsColumns(){
-            int rowCnt, columnCnt, finalSum=0;
+            int rowCnt, columnCnt,right2leftDiagonal,left2rightDiagonal ,finalSum=0;
             for (int i = 0; i < SIZE; i++ ){ // for each row.
                 rowCnt = checkRow(i);                    
                 for (int j = 0; j < SIZE; j++){
@@ -82,6 +82,17 @@ namespace Leonardo
                     finalSum += columnCnt;
                 }
             }
+            right2leftDiagonal = checkRight2LeftDiagonal();
+            left2rightDiagonal = checkLeft2RightDiagonal();
+            if (right2leftDiagonal > 0)
+            {
+                blankRight2LeftDiagonal();
+            }
+            if (left2rightDiagonal > 0)
+            {
+                blankLeft2RightDiagonal();
+            } 
+
             return finalSum;
         }
 
@@ -92,11 +103,7 @@ namespace Leonardo
         private void blankRow(int i)
         {
             for (int k = 0; k < SIZE; k++){
-                gameBoard[i, k].ImageButton.SetImageResource(Resource.Drawable.blank);
-                gameBoard[i, k].Shape = "blank";
-                gameBoard[i, k].Color = "white";
-                gameBoard[i, k].Amount = 0;
-                gameBoard[i, k].ImageButton.Enabled = true;
+                blankOnIandJ(i, k);
             }
         }
 
@@ -107,15 +114,36 @@ namespace Leonardo
         private void blankColumn(int j)
         {
             for (int k = 0; k < SIZE;k++ ){
-                gameBoard[k, j].ImageButton.SetImageResource(Resource.Drawable.blank);
-                gameBoard[k, j].Shape = "blank";
-                gameBoard[k, j].Color = "white";
-                gameBoard[k, j].Amount = 0;
-                gameBoard[k, j].ImageButton.Enabled = true;
+                blankOnIandJ(k, j);
             }
         }
 
-  
+        private void blankLeft2RightDiagonal()
+        {
+            for (int i = SIZE - 1; i >= 0; i--)
+            {
+                blankOnIandJ(i, SIZE - 1 - i);
+            }
+            
+        }
+
+        private void blankRight2LeftDiagonal()
+        {
+            for (int i = 0; i < SIZE; i++)
+            {
+                blankOnIandJ(i, i);
+            }
+        }
+
+        private void blankOnIandJ(int i,int j)
+        {
+            gameBoard[i, j].ImageButton.SetImageResource(Resource.Drawable.blank);
+            gameBoard[i, j].Shape = "blank";
+            gameBoard[i, j].Color = "white";
+            gameBoard[i, j].Amount = 0;
+            gameBoard[i, j].ImageButton.Enabled = true;
+        }
+
         /// <summary>
         ///     Check an individual column.
         /// </summary>
@@ -184,24 +212,9 @@ namespace Leonardo
             }
 
             // If bonus needed.
-            if (columnSuccess == 3)
-            {
+            if (columnSuccess == 3){
                 sum *= 2;
             }
-
-            // If at least 1 row exists.
-           /*
-            if (columnSuccess > 0){
-                gameSuccess++;
-                for (int j = 0; j < SIZE; j++)
-                {
-                    gameBoard[j, i].ImageButton.SetImageResource(Resource.Drawable.blank);
-                    gameBoard[j, i].Shape = "blank";
-                    gameBoard[j, i].Color = "white";
-                    gameBoard[j, i].Amount = 0;
-                    gameBoard[j, i].ImageButton.Enabled = true;
-                }
-            }*/
             return sum;
         }
 
@@ -273,24 +286,120 @@ namespace Leonardo
             }
 
             // If bonus needed.
-            if (rowSuccess == 3)
-            {
+            if (rowSuccess == 3){
                 sum *= 2;
             }
-
-            // If at least 1 row exists.
-          /*  if (rowSuccess > 0)
-            {
-                for (int j = 0; j < SIZE; j++)
-                {
-                    gameBoard[i, j].ImageButton.SetImageResource(Resource.Drawable.blank);
-                    gameBoard[i, j].Shape = "blank";
-                    gameBoard[i, j].Color = "white";
-                    gameBoard[i, j].Amount = 0;
-                    gameBoard[i, j].ImageButton.Enabled = true;
-                }
-            }*/
             return sum;
         }
+
+        private int checkRight2LeftDiagonal()
+        {
+            int sum=0,tempInt,cnt=0,diagonalSuccess=0;
+            string tempStr;
+            for (int j = 0; j < SIZE; j++){ //checked with the rest.
+                if (gameBoard[j, j].Shape == "blank"){
+                    // at least 1 blank exists.
+                    return 0;
+                }
+            }
+            // Check amount.
+            tempInt = gameBoard[0, 0].Amount; //took the first.
+            for (int j = 1; j < SIZE; j++){ //checked with the rest.
+                if (gameBoard[j, j].Amount == tempInt){
+                    cnt++;
+                }
+            }
+            if (cnt == 3){ 
+                sum += 50;
+            }
+            // Check color.
+            cnt = 0;
+            tempStr = gameBoard[0, 0].Color; //took the first.
+            for (int j = 1; j < SIZE; j++){ //checked with the rest.
+                if (gameBoard[j, j].Color == tempStr){
+                    cnt++;
+                }
+            }
+            if (cnt == 3){
+                sum += 50;
+            }
+            // Check shape.
+            cnt = 0;
+            tempStr = gameBoard[0, 0].Shape; //took the first.
+            for (int j = 1; j < SIZE; j++){ //checked with the rest.
+                if (gameBoard[j, j].Shape == tempStr){
+                    cnt++;
+                }
+            }
+            if (cnt == 3){ //Successfull row.
+                sum += 50;
+                diagonalSuccess += 1;
+            }
+
+            // If bonus needed.
+            if (diagonalSuccess == 3){
+                sum *= 2;
+            }
+            return sum;
+        }
+
+        private int checkLeft2RightDiagonal()
+        {
+            int sum = 0, tempInt, cnt = 0, diagonalSuccess = 0;
+            string tempStr;
+            for (int j = 0; j < SIZE ; j++)
+            { //checked with the rest.
+                if (gameBoard[j, SIZE - 1 - j].Shape == "blank")
+                {
+                    // at least 1 blank exists.
+                    return 0;
+                }
+            }
+            // Check amount.
+            tempInt = gameBoard[0, SIZE-1].Amount; //took the first.
+            for (int j = 1; j < SIZE; j++)
+            { //checked with the rest.
+                if (gameBoard[j, SIZE - 1 - j].Amount == tempInt)
+                {
+                    cnt++;
+                }
+            }
+            if (cnt == 3){
+                sum += 50;
+            }
+            // Check color.
+            cnt = 0;
+            tempStr = gameBoard[0, SIZE - 1].Color; //took the first.
+            for (int j = 1; j < SIZE; j++){
+                if (gameBoard[j, SIZE - 1 - j].Color == tempStr)
+                {
+                    cnt++;
+                }
+            }
+            if (cnt == 3)
+            {
+                sum += 50;
+            }
+            // Check shape.
+            cnt = 0;
+            tempStr = gameBoard[0, SIZE - 1].Shape; //took the first.
+            for (int j = 1; j < SIZE; j++){
+                if (gameBoard[j, SIZE - 1 - j].Shape == tempStr)
+                {
+                    cnt++;
+                }
+            }
+            if (cnt == 3){ //Successfull diagonal.
+                sum += 50;
+                diagonalSuccess += 1;
+            }
+
+            // If bonus needed.
+            if (diagonalSuccess == 3){
+                sum *= 2;
+            }
+            return sum;
+        }
+
     }
 }
